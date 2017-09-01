b3012bc9a0d1ebb5db1271b960f1f6727654c19b
KRISHNASAMI SREEDHARAN <krishnasami.sreedharan@gmail.com>
2017-09-02 03:02:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,15 +4,9 @@ element.innerHTML="Welcome to my World";
 var img=document.getElementById("img-medium")
 var marginLeft=0;
 function moveRight(){
-    if(marginLeft===0){
     marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft + "px" ;
     }
-    if(marginLeft>400){
-        marginLeft-1;
-        img.style.marginLeft=marginLeft + "px" ;
-    }
-    }
 img.onclick=function(){
     var interval=setInterval(moveRight,50);
-};
+}
