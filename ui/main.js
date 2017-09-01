c0c0114806593baec1d7087ba626e1dfb8cb223c
KRISHNASAMI SREEDHARAN <krishnasami.sreedharan@gmail.com>
2017-09-02 03:04:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,9 @@ var marginLeft=0;
 function moveRight(){
     marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft + "px" ;
+    if(marginLeft===100){
+        marginLeft=0;
+    }
     }
 img.onclick=function(){
     var interval=setInterval(moveRight,50);
