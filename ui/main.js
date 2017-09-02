f847f35c704d834345056c634b0479912b24732f
KRISHNASAMI SREEDHARAN <krishnasami.sreedharan@gmail.com>
2017-09-02 16:06:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,27 +3,20 @@ var button=document.getElementById('counter');
 button.onclick=function(){
     
     //create a request object
-    var request=new XMLHttpRequest();
-    
-    //capture response and store it in a variable.
-    request.onreadystatechange = function(){
-        if(request.readystate === XMLHttpRequest.DONE){
-            //take some action
-            if(Request.status === 200){
-                var counter1=request.responseText;
-                var span=document.getElementById('count');
-                span.innerHTML = counter1.toString();
-                console.log("counter Was Sent To Span");
-            }
-            else 
-            { 
-                alert('There was a problem with the request.'); 
-                
-            }
-
-        }
-    //not done yet
-    };
+ var request = new XMLHttpRequest();
+    request.onreadystatechange = function() 
+    {
+          if (request.readyState === XMLHttpRequest.DONE) 
+          {
+              //Take Some Action
+              if (request.status === 200) 
+                { 
+                    var counter = request.responseText; 
+                    var span = document.getElementById('count');
+                    span.innerHTML = counter.toString();
+                } 
+          } 
+    }; 
  //make the request   
  request.open('GET','http://krishnasamis2014it.imad.hasura-app.io/counter',true);
  request.send(); 
