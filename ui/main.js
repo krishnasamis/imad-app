1a1d0af99513049b02e84627360624c0bebd6b42
KRISHNASAMI SREEDHARAN <krishnasami.sreedharan@gmail.com>
2017-09-02 19:00:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ var button=document.getElementById('counter');
 
 button.onclick=function(){
     
-    //create a request object
+//create a request object
  var request = new XMLHttpRequest();
     request.onreadystatechange = function() 
     {
@@ -21,25 +21,40 @@ button.onclick=function(){
  request.open('GET','http://krishnasamis2014it.imad.hasura-app.io/counter',true);
  request.send(); 
     console.log('EXECUTED');
-}
+};
 
 //submit name
 var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
-    //make a request to server and send the name
-    //capture the list of names and render it.
-    var names=['name1','name2','name3'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>' +names[i]+ '</li>';
-    }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+    
+        //create a request object
+ var request = new XMLHttpRequest();
+    request.onreadystatechange = function() 
+    {
+          if (request.readyState === XMLHttpRequest.DONE) 
+          {
+              //Take Some Action
+              if (request.status === 200) 
+                { 
+                    //make a request to server and send the name
+                    //capture the list of names and render it.
+                    var names=request.responseText;
+                    names=JSON.parse(names);
+                    var list='';
+                    for(var i=0;i<names.length;i++){
+                        list+='<li>' +names[i]+ '</li>';
+                    }
+                    var ul=document.getElementById('namelist');
+                    ul.innerHTML=list;
+                } 
+          } 
+    }; 
+ //make the request   
+ request.open('GET','http://krishnasamis2014it.imad.hasura-app.io/submit-name?name='+name,true);
+ request.send(); 
+    console.log('EXECUTED');
 };
-    
-    
-}
 
 
